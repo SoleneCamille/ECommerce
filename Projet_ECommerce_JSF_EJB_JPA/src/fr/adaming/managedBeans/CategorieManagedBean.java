71ba8239c03bab63a696ae625e8cb4d10bcee489
Adaming44 <Adaming44@192.168.1.145>
2018-01-08 14:42:34 +0100

    camille : primefaces

---
+++ Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/managedBeans/CategorieManagedBean.java
@@ -0,0 +1,63 @@
+package fr.adaming.managedBeans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import fr.adaming.model.Categorie;
+import fr.adaming.service.ICategorieService;
+
+@ManagedBean(name = "catMB")
+@RequestScoped
+public class CategorieManagedBean implements Serializable {
+
+	// transformation de l'association UML en java
+	@EJB
+	private ICategorieService categorieService;
+
+	private Categorie categorie;
+	private List<Categorie> listeCategories;
+
+	private HttpSession maSession;
+
+	public CategorieManagedBean() {
+		this.categorie = new Categorie();
+	}
+
+	// méthode qui s'exécute après l'instanciation du managedBean
+	@PostConstruct
+	public void init() {
+		this.maSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+	}
+
+	//getters et setters
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	public List<Categorie> getListeCategories() {
+		return listeCategories;
+	}
+
+	public void setListeCategories(List<Categorie> listeCategories) {
+		this.listeCategories = listeCategories;
+	}
+
+	public void setCategorieService(ICategorieService categorieService) {
+		this.categorieService = categorieService;
+	}
+	
+	//les méthodes métiers
+	
+
+}
