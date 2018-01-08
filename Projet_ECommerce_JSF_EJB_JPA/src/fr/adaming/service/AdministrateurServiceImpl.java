b488348f25206baa8eeeeb795eaec615d6364108
inti-0257 <inti-0257@192.168.1.167>
2018-01-08 14:27:25 +0100

    Solene: agent

--- Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/service/AdministrateurServiceImpl.java
+++ Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/service/AdministrateurServiceImpl.java
@@ -1,14 +1,36 @@
 package fr.adaming.service;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+
+import fr.adaming.dao.IAdministrateurDao;
 import fr.adaming.model.Administrateur;
 
-
+@Stateful
 public class AdministrateurServiceImpl implements IAdministrateurService{
+
+	@EJB //pour injecter DAO
+	private IAdministrateurDao adminDao;
+	
+	
+	//création de getters et setters nécessaires à l'injection dépendance
+	public IAdministrateurDao getAdminDao() {
+		return adminDao;
+	}
+
+
+
+	public void setAgentDao(IAdministrateurDao adminDao) {
+		this.adminDao = adminDao;
+	}
+
+
 
 	@Override
 	public Administrateur isExist(Administrateur a) throws Exception {
-
-		return null;
+		//appel de la méthode DAO
+		return adminDao.isExist(a);
 	}
+
 
 }
