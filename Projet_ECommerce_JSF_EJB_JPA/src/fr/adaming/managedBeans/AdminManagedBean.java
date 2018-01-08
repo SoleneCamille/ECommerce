2a4bbadf7760cf73a04fbfcae7eb98a29c61bcef
Adaming44 <Adaming44@192.168.1.145>
2018-01-08 16:14:45 +0100

    camille

--- Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/managedBeans/AdminManagedBean.java
+++ Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/managedBeans/AdminManagedBean.java
@@ -3,25 +3,21 @@ package fr.adaming.managedBeans;
 import java.io.Serializable;
 import java.util.List;
 
-
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
-
 import fr.adaming.model.Administrateur;
 import fr.adaming.model.Categorie;
 import fr.adaming.service.IAdministrateurService;
 import fr.adaming.service.ICategorieService;
 
-
-@ManagedBean(name="aMB")
+@ManagedBean(name = "aMB")
 @RequestScoped
 public class AdminManagedBean implements Serializable {
-	
-	
+
 	@EJB
 	private IAdministrateurService adminService;
 
@@ -30,80 +26,69 @@ public class AdminManagedBean implements Serializable {
 
 	private Administrateur admin;
 	private List<Categorie> listeCategories;
-	
-	//constructeur vide
+
+	// constructeur vide
 	public AdminManagedBean() {
 		super();
 	}
 
-	//getters & setters
+	// getters & setters
 
 	public IAdministrateurService getAdminService() {
 		return adminService;
 	}
 
-
 	public void setAdminService(IAdministrateurService adminService) {
 		this.adminService = adminService;
 	}
-
 
 	public ICategorieService getCategorieService() {
 		return categorieService;
 	}
 
-
 	public void setCategorieService(ICategorieService categorieService) {
 		this.categorieService = categorieService;
 	}
-
 
 	public Administrateur getAdmin() {
 		return admin;
 	}
 
-
 	public void setAdmin(Administrateur admin) {
 		this.admin = admin;
 	}
-
 
 	public List<Categorie> getListeCategories() {
 		return listeCategories;
 	}
 
-
 	public void setListeCategories(List<Categorie> listeCategories) {
 		this.listeCategories = listeCategories;
 	}
 
-public String seConnecter() {
-		
+	public String seConnecter() {
+
 		try {
-		Administrateur aOut = adminService.isExist(this.admin);
-			
-			//récup de la liste de categories
+			Administrateur aOut = adminService.isExist(this.admin);
+
+			// récup de la liste de categories
 			listeCategories = categorieService.getAllCategories();
-			
-			//ajouter la liste dans la session
-			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("categoriesList", listeCategories);
-			
-			//ajouter l'admin dans la session
+
+			// ajouter la liste dans la session
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("categoriesList",
+					listeCategories);
+
+			// ajouter l'admin dans la session
 			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("adminSession", aOut);
-			
+
 			return "accueilAdmin";
-			
-		} catch (Exception e){
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("L'identifiant ou le mot de passe est incorrect"));
-			
+
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("L'identifiant ou le mot de passe est incorrect"));
+
 		}
 		return "login";
 	}
-
-public String renvoiLogin(){
-	return "login";
-}
-	
-	
 
 }
