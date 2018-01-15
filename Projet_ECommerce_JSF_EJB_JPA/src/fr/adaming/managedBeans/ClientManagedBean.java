063e56b4c63b399210d5468e75a451b716d77e1b
inti-0257 <inti-0257@192.168.1.167>
2018-01-15 10:27:33 +0100

    Solene: envoi mail

--- Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/managedBeans/ClientManagedBean.java
+++ Projet_ECommerce_JSF_EJB_JPA/src/fr/adaming/managedBeans/ClientManagedBean.java
@@ -272,83 +272,88 @@ public class ClientManagedBean implements Serializable {
 	public void envoiMail() throws AddressException, MessagingException, FileNotFoundException, DocumentException{
 		//création pdf
 			   
-		/* Create a new Document object */
-		Document document = new Document();
-		
-		try {
-		    /* Associate the document with a PDF writer and an output stream */
-		    PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\inti-0257\\Desktop\\formation\\Commande.pdf"));
-	 
-		    /* Open the document (ready to add items) */
-		    document.open();
-	 
-		    /* Populate the document (add items to it) */ 
-//		    Commande comDefaut = new Commande();
-//			comDefaut.setIdCommande(1);
-			List<LignesCommande> liste=ligneService.getAllLignes(this.commande.getIdCommande());
-			
-		document.add(new Paragraph("Récapitulatif de votre commande"));
-		
-		
-		PdfPTable table = new PdfPTable(5);
+//		/* Create a new Document object */
+//		Document document = new Document();
+//		
+//		try {
+//		    /* Associate the document with a PDF writer and an output stream */
+//		    PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\inti-0257\\Desktop\\formation\\Commande.pdf"));
+//	 
+//		    /* Open the document (ready to add items) */
+//		    document.open();
+//	 
+//		    /* Populate the document (add items to it) */ 
+////		    Commande comDefaut = new Commande();
+//
+//		    System.out.println("###################");
+//			List<LignesCommande> liste=ligneService.getAllLignes(23);
+//			
+//		document.add(new Paragraph("Récapitulatif de votre commande"));
+//		
+//		
+//		PdfPTable table = new PdfPTable(5);
+////	      
+////	      //On créer l'objet cellule.
+//	      PdfPCell cell;
+////	      
+//	      cell = new PdfPCell(new Phrase("Liste des produits commandés"));
+//	      cell.setColspan(5);
+//	      table.addCell(cell);
 //	      
-//	      //On créer l'objet cellule.
-	      PdfPCell cell;
+//	      table.addCell("nom du produit");
+//	      table.addCell("quantité");
+//	      table.addCell("prix avant remise");
+//	      table.addCell("remise");
+//	      table.addCell("prix après remise");
 //	      
-	      cell = new PdfPCell(new Phrase("Liste des produits commandés"));
-	      cell.setColspan(5);
-	      table.addCell(cell);
-	      
-	      table.addCell("nom du produit");
-	      table.addCell("quantité");
-	      table.addCell("prix avant remise");
-	      table.addCell("remise");
-	      table.addCell("prix après remise");
-	      
-	 
-	     // cell = new PdfPCell(new Phrase("Fusion de 2 cellules de la première colonne"));
-	      //cell.setRowspan(3);
-	      //table.addCell(cell);
-	 
-//	      //contenu du tableau.
-//	      table.addCell(liste.get(0).getProduit().getDesignation());
-//	      table.addCell(Integer.toString(liste.get(0).getQuantite()));
-//	      table.addCell(Double.toString(liste.get(0).getPrixAvantRemise()));
-//	      table.addCell(Double.toString(liste.get(0).getProduit().getRemise()));
-//	      table.addCell(Double.toString(liste.get(0).getPrix()));
-	      
-	      document.add(table);
-	      
-	      
-	      
-	      PdfPTable table2 = new PdfPTable(2);
-	   
-      
-	      cell = new PdfPCell(new Phrase("Montant total"));
-	      cell.setColspan(2);
-	      table2.addCell(cell);
-	      
-	      table2.addCell("prix total avant remise");
-	      table2.addCell("prix total après remise");
-	     
-//	      table.addCell(Double.toString(this.commande.getPrixAvant()));
-//	      table.addCell(Double.toString(this.commande.getPrixApres()));
-      
-	      document.add(table2);
-		
-		System.out.println("pdf cree");
-		}
-		catch(DocumentException e) {
-		    /* Oups */
-		    System.err.println(e);
-		}
-		finally {
-		    /* Don't forget to close the document! */
-		    document.close();
-		   
-		}
-	    
-	 
+//	 
+//	     // cell = new PdfPCell(new Phrase("Fusion de 2 cellules de la première colonne"));
+//	      //cell.setRowspan(3);
+//	      //table.addCell(cell);
+//	 
+////	      //contenu du tableau.
+//	      System.out.println("###################");
+//	      
+//	      for(int i=0;i<liste.size();i++){
+//	      //System.out.println(liste.get(0).getProduit().getDesignation());
+//	      table.addCell(liste.get(i).getProduit().getDesignation());
+//	      table.addCell(Integer.toString(liste.get(i).getQuantite()));
+//	      table.addCell(Double.toString(liste.get(i).getPrixAvantRemise()));
+//	      table.addCell(Double.toString(liste.get(i).getProduit().getRemise()));
+//	      table.addCell(Double.toString(liste.get(i).getPrix()));}
+//	      
+//	      document.add(table);
+//	      
+//	      
+//	      
+//	      PdfPTable table2 = new PdfPTable(2);
+//	   
+//      
+//	      cell = new PdfPCell(new Phrase("Montant total"));
+//	      cell.setColspan(2);
+//	      table2.addCell(cell);
+//	      
+//	      table2.addCell("prix total avant remise");
+//	      table2.addCell("prix total après remise");
+//	     
+////	      table.addCell(Double.toString(this.commande.getPrixAvant()));
+////	      table.addCell(Double.toString(this.commande.getPrixApres()));
+//      
+//	      document.add(table2);
+//		
+//		System.out.println("pdf cree");
+//		}
+//		catch(DocumentException e) {
+//		    /* Oups */
+//		    System.err.println(e);
+//		}
+//		finally {
+//		    /* Don't forget to close the document! */
+//		    document.close();
+//		   
+//		}
+//	    
+//	 
 		//Envoi du mail contenant le pdf	
 
         Properties props = System.getProperties();
@@ -369,7 +374,7 @@ public class ClientManagedBean implements Serializable {
         multipart.addBodyPart(messageBodyPart);
 
         messageBodyPart = new MimeBodyPart();
-        DataSource source = new FileDataSource("C:\\Users\\inti-0257\\Desktop\\formation\\Commande.pdf");
+        DataSource source = new FileDataSource("C:\\Users\\inti-0257\\Downloads\\Commande.pdf");
         messageBodyPart.setDataHandler(new DataHandler(source));
         messageBodyPart.setFileName("commande.pdf");
         multipart.addBodyPart(messageBodyPart);
